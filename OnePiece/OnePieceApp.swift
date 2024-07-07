a3c43cdb43f447283fa675dcb8c40c462474117b
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-07-07 19:04:48 +0530

    Initial Commit

---
+++ OnePiece/OnePieceApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePieceApp.swift
+//  OnePiece
+//
+//  Created by Manik Lakhanpal on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct OnePieceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
